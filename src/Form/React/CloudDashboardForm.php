c404a136cc8b579c04a3f2b8eae0a225cd332292
Ryo Yamashita <yamashita@ipride.co.jp>
2021-07-07 16:49:32 +0900

    Add custom module files

---
+++ src/Form/React/CloudDashboardForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\cloud_dashboard\Form\React;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class CloudReactForm.
+ *
+ * @package Drupal\cloud_dashboard\Form\React
+ */
+class CloudDashboardForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'cloud_dashboard';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['html'] = [
+      '#markup' => '<div id="root"></div>',
+      '#attached' => [
+        'library' => 'cloud_dashboard/react_app',
+      ],
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+  }
+
+}
