7784956d43e859dba335fc99a3911bf952cca754
Ryo Yamashita <48265860+RyoYamashita765@users.noreply.github.com>
2021-08-06 13:33:50 +0900

    CLOUD-2561: Add the AWS instance list view and tab menu (#4)
    * Add AWS instance list view
    
    * Change symbol name

--- cloud_dashboard/src/container/LoginForm.tsx
+++ cloud_dashboard/src/container/LoginForm.tsx
@@ -36,13 +36,15 @@ const LoginForm: React.VFC = () => {
     window.localStorage.setItem('redirectUri', redirectUri);
   }, [redirectUri]);
 
-  return (
-    <form>
-      <button type="button" className="btn btn-default"
-        onClick={login}
-        disabled={clientId === ''}>login</button>
-    </form>
-  );
+  return <div className="row">
+    <div className="col">
+      <form>
+        <button type="button" className="btn btn-default"
+          onClick={login}
+          disabled={clientId === ''}>login</button>
+      </form>
+    </div>
+  </div>;
 }
 
 export default LoginForm;
