de329a8fad8bf5c2d1b03a058ff3e63d99a28d21
Ryo Yamashita <48265860+RyoYamashita765@users.noreply.github.com>
2021-08-27 11:06:33 +0900

    Add K8s node list view and refactor code (#6)
    * Add K8s node list view and refactor code
    
    * Refactor code
    
    * Fix if statements

--- cloud_dashboard/src/App.tsx
+++ cloud_dashboard/src/App.tsx
@@ -4,8 +4,8 @@ import { ROUTE_URL } from 'constant';
 import CallbackView from 'container/Callback';
 import LoginForm from 'container/LoginForm';
 import MainForm from 'container/MainForm';
-import K8sPodForm from 'container/K8sPodForm';
 import AwsCloudInstanceForm from 'container/AwsInstanceForm';
+import K8sEntityForm from 'container/K8sEntityForm';
 
 const App: React.VFC = () => {
   return <BrowserRouter basename={ROUTE_URL}>
@@ -21,10 +21,42 @@ const App: React.VFC = () => {
           <CallbackView />
         </div>
       </Route>
+      <Route exact path="/k8s/node">
+        <div className="container">
+          <MainForm selectedMenuName="K8s Node"/>
+          <K8sEntityForm
+            entityTypeId="k8s_node"
+            namespaceFlg={false}
+            column={[
+              {labelName: 'Name', name: 'name', type: 'default'},
+              {labelName: 'State', name: 'status', type: 'default'},
+              {labelName: 'CPU (Request)', name: 'cpu_request', type: 'default'},
+              {labelName: 'CPU (Limit)', name: 'cpu_limit', type: 'default'},
+              {labelName: 'CPU (Usage)', name: 'cpu_usage', type: 'default'},
+              {labelName: 'Memory (Request)', name: 'memory_request', type: 'memory'},
+              {labelName: 'Memory (Limit)', name: 'memory_limit', type: 'memory'},
+              {labelName: 'Memory (Usage)', name: 'memory_usage', type: 'memory'},
+              {labelName: 'Pods (Applcation)', name: 'pods_allocation', type: 'default'},
+              {labelName: 'Created', name: 'created', type: 'datetime'}
+            ]} />
+        </div>
+      </Route>
       <Route exact path="/k8s/pod">
         <div className="container">
           <MainForm selectedMenuName="K8s Pod"/>
-          <K8sPodForm />
+          <K8sEntityForm
+            entityTypeId="k8s_pod"
+            namespaceFlg={true}
+            column={[
+              {labelName: 'Namespace', name: 'namespace', type: 'default'},
+              {labelName: 'Name', name: 'name', type: 'default'},
+              {labelName: 'Node', name: 'node_name', type: 'default'},
+              {labelName: 'State', name: 'status', type: 'default'},
+              {labelName: 'Restarts', name: 'restarts', type: 'default'},
+              {labelName: 'CPU (Usage)', name: 'cpu_usage', type: 'default'},
+              {labelName: 'Memory (Usage)', name: 'memory_usage', type: 'default'},
+              {labelName: 'Created', name: 'created', type: 'datetime'}
+            ]} />
         </div>
       </Route>
       <Route exact path="/">
