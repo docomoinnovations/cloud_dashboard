862b3483849359cba884964d5660f0ea8c6a36b1
Ryo Yamashita <yamashita@ipride.co.jp>
2021-10-14 12:09:35 +0900

    Add refresh token function

--- cloud_dashboard/src/container/MenuBar.tsx
+++ cloud_dashboard/src/container/MenuBar.tsx
@@ -1,9 +1,40 @@
-import { AWS_MENU_LIST, K8S_MENU_LIST, ROUTE_URL } from 'constant';
+import { AWS_MENU_LIST, K8S_MENU_LIST, OAUTH2_CLIENT_SECRET, ROUTE_URL } from 'constant';
 import CloudContext from 'model/CloudContext';
 import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { AppContext } from 'service/state';
 import { getUrl } from 'service/utility';
+
+const refreshTokenByCodeGrant = async (clientId: string, refreshToken: string) => {
+  const formData = new FormData();
+  formData.append('grant_type', 'refresh_token');
+  formData.append('client_id', clientId);
+  formData.append('client_secret', OAUTH2_CLIENT_SECRET);
+  formData.append('refresh_token', refreshToken);
+
+  const res = await fetch(`/oauth/token`, {
+    method: 'POST',
+    body: formData
+  });
+
+  if (!res.ok) {
+    throw new Error('Refresh failed');
+  }
+  const res2 = await res.json();
+  if ('access_token' in res2) {
+    // read tokens
+    const accessToken = res2['access_token'];
+    const refreshToken2 = res2['refresh_token'];
+    const expiresDatetime = (new Date()).getTime() + res2['expires_in'] * 1000;
+
+    // save tokens to Localstrage
+    window.localStorage.setItem('accessToken', accessToken);
+    window.localStorage.setItem('refreshToken', refreshToken2);
+    window.localStorage.setItem('expiresDatetime', `${expiresDatetime}`);
+  } else {
+    throw new Error('Refresh failed');
+  }
+};
 
 const MenuBar: React.VFC = () => {
   const { cloudContext, cloudContextList, dispatch } = useContext(AppContext);
@@ -17,10 +48,34 @@ const MenuBar: React.VFC = () => {
       return;
     }
 
-    // If the access token has expired, redirect route URL.
+    // If the access token has expired, update it.
+    console.group('Authorization Code Grant');
+    const clientId = window.localStorage.getItem('clientId');
+    const refreshToken = window.localStorage.getItem('refreshToken');
+    if (clientId === null || refreshToken === null) {
+      console.log('Client ID : No');
+      console.error('Authorization failed.');
+      console.groupEnd();
+      window.location.href = ROUTE_URL;
+      return;
+    }
+    console.log('Client ID : Yes');
+
     const now = (new Date()).getTime();
     if (now > parseInt(expiresDatetime, 10)) {
-      window.location.href = ROUTE_URL;
+      console.log('Token expired : Yes');
+      refreshTokenByCodeGrant(clientId, refreshToken).then(() => {
+        console.log('Access token : Yes');
+        console.groupEnd();
+      }).catch(() => {
+        console.log('Access token : No');
+        console.error('Authorization failed.');
+        console.groupEnd();
+        window.location.href = ROUTE_URL;
+      });
+    } else {
+      console.log('Token expired : No');
+      console.groupEnd();
     }
   }, []);
 
