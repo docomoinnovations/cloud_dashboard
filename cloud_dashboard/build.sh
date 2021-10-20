48c4ca9a86901462314d39501d175ab797f941d2
Ryo Yamashita <48265860+RyoYamashita765@users.noreply.github.com>
2021-10-20 12:21:33 +0900

    Add map view in CloudServiceProvider list view (#23)
    * Add sample map view in provider view by leaflet.js
    
    * Add loading icon code
    
    * Add Markers of cloud_context in the map view

--- cloud_dashboard/build.sh
+++ cloud_dashboard/build.sh
@@ -3,7 +3,9 @@ rm ../js/2.chunk.js
 rm ../js/3.chunk.js
 rm ../js/main.chunk.js
 rm ../js/runtime-main.js
+rm ../css/2.chunk.css
 cp build/static/js/2.*.chunk.js ../js/2.chunk.js
 cp build/static/js/3.*.chunk.js ../js/3.chunk.js
 cp build/static/js/main.*.chunk.js ../js/main.chunk.js
 cp build/static/js/runtime-main.*.js ../js/runtime-main.js
+cp build/static/css/2.*.chunk.css ../css/2.chunk.css
--- cloud_dashboard/src/container/ProviderView.tsx
+++ cloud_dashboard/src/container/ProviderView.tsx
@@ -1,13 +1,70 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AppContext } from 'service/state';
 import { useTranslation } from 'react-i18next';
 import { LANGUAGE_LIST } from 'i18n';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet, { LatLngTuple } from 'leaflet';
+import HttpService from 'service/http';
+
+Leaflet.Icon.Default.imagePath =
+  '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/';
+
+interface CloudContetLocation {
+  name: string;
+  position: LatLngTuple;
+}
 
 const ProviderView: React.VFC = () => {
   const { cloudContextList, dispatch } = useContext(AppContext);
+  const [cloudContetLocationList, setCloudContetLocationList] = useState<CloudContetLocation[]>([]);
   const { t, i18n } = useTranslation();
 
+  useEffect(() => {
+    const http = HttpService.getInstance();
+    http.getJson<{
+      Items: {
+        Image: string,
+        Name: string
+      }[],
+      Latitude: string,
+      Longitude: string
+    }[]>('/clouds/cloud_config_location').then((res) => {
+      const newList: CloudContetLocation[] = [];
+      for (const item of res) {
+        for (const item2 of item.Items) {
+          newList.push({
+            name: item2.Name,
+            position: [
+              parseFloat(item.Latitude),
+              parseFloat(item.Longitude)
+            ]
+          });
+        }
+      }
+      setCloudContetLocationList(newList);
+    });
+  }, []);
+
   return <div className="container-fluid">
+    <div className="row">
+      <div className="col">
+        <MapContainer center={[0, 0]} zoom={2} scrollWheelZoom={false} style={{ height: 500 }}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {
+            cloudContetLocationList.map((cloudContetLocation, index) => {
+              return <Marker position={cloudContetLocation.position} key={index}>
+                <Popup>
+                  {cloudContetLocation.name}
+                </Popup>
+              </Marker>;
+            })
+          }
+        </MapContainer>
+      </div>
+    </div>
     <div className="row">
       <div className="col">
         <form>
@@ -21,8 +78,8 @@ const ProviderView: React.VFC = () => {
                     className="btn btn-default"
                     disabled={i18n.language === language.key}
                     onClick={() => {
-                      dispatch({type: 'setLanguage', message: language.key});
-                  }}>{language.value}</button>;
+                      dispatch({ type: 'setLanguage', message: language.key });
+                    }}>{language.value}</button>;
                 })
               }
             </div>
@@ -40,10 +97,10 @@ const ProviderView: React.VFC = () => {
                   cloudContextList
                     .filter((r) => r.name !== 'ALL')
                     .map((r, index) => {
-                    return <tr key={index}>
-                    <td>{r.labelName}</td>
-                  </tr>;
-                  })
+                      return <tr key={index}>
+                        <td>{r.labelName}</td>
+                      </tr>;
+                    })
                 }
               </tbody>
             </table>
