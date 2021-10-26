33e07da7789a1015830eccf3bdafae4330d975dc
Ryo Yamashita <yamashita@ipride.co.jp>
2021-10-26 17:28:26 +0900

    Update Popup tag on Marker tag

--- cloud_dashboard/src/container/ProviderView.tsx
+++ cloud_dashboard/src/container/ProviderView.tsx
@@ -3,22 +3,68 @@ import { AppContext } from 'service/state';
 import { useTranslation } from 'react-i18next';
 import { LANGUAGE_LIST } from 'i18n';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import Leaflet from 'leaflet';
+import Leaflet, { LatLngTuple } from 'leaflet';
 import RawCloudContextItem from 'model/RawCloudContextItem';
 import { loadRawCloudContextItemList } from 'service/utility';
+import L from 'leaflet';
+import { ROUTE_URL } from 'constant';
+
+interface CloudContenxtItem {
+  icon: L.Icon | undefined,
+  position: LatLngTuple,
+  item: {
+    iconUrl: string,
+    entityViewUrl: string,
+    name: string,
+    positionLabel: string,
+  }[]
+}
 
 Leaflet.Icon.Default.imagePath =
   '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/';
 
+const convert = (rawCloudContenxtItemList: RawCloudContextItem[]): CloudContenxtItem[] => {
+  const hash: Record<string, CloudContenxtItem> = {};
+  for (const rawCloudContenxtItem of rawCloudContenxtItemList) {
+    const hashKey = `${rawCloudContenxtItem.Latitude},${rawCloudContenxtItem.Longitude}`;
+    if (hashKey in hash) {
+      hash[hashKey].icon = undefined;
+    } else {
+      hash[hashKey] = {
+        icon: new L.Icon({
+          iconUrl: rawCloudContenxtItem.Items[0].Image
+        }),
+        position: [
+          parseFloat(rawCloudContenxtItem.Latitude),
+          parseFloat(rawCloudContenxtItem.Longitude),
+        ],
+        item: []
+      };
+    }
+
+    const itemList = rawCloudContenxtItem.Items.map((rawItem) => {
+      return {
+        iconUrl: rawItem.Image,
+        entityViewUrl: rawCloudContenxtItem.Type === 'aws_cloud'
+          ? ROUTE_URL + '/aws_cloud/instance'
+          : ROUTE_URL + '/k8s/node',
+        name: rawItem.Name,
+        positionLabel: `${rawCloudContenxtItem.City}, ${rawCloudContenxtItem.Country}`,
+      };
+    });
+    hash[hashKey].item = [...hash[hashKey].item, ...itemList];
+  }
+  return Object.values(hash);
+}
+
 const ProviderView: React.VFC = () => {
   const { cloudContextList, dispatch } = useContext(AppContext);
-  const [rawCloudContenxtItemList, setRawCloudContenxtItemList] = useState<RawCloudContextItem[]>([]);
+  const [cloudContenxtItemList, setCloudContenxtItemList] = useState<CloudContenxtItem[]>([]);
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
     loadRawCloudContextItemList().then((res) => {
-      console.log(res);
-      setRawCloudContenxtItemList(res);
+      setCloudContenxtItemList(convert(res));
     });
   }, []);
 
@@ -31,32 +77,29 @@ const ProviderView: React.VFC = () => {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           {
-            rawCloudContenxtItemList.map((item, index) => {
-              return <Marker position={[
-                parseFloat(item.Latitude),
-                parseFloat(item.Longitude),
-              ]} key={index}>
+            cloudContenxtItemList.map((item, index) => {
+              return <Marker position={item.position} icon={item.icon} key={index}>
                 <Popup>
                   {
-                    item.Items.map((item2, index2) => {
+                    item.item.map((item2, index2) => {
                       return <div key={index2}>
                         <div>
-                          <a href={item2.Url} target="_blank" rel="noreferrer">
-                            <img src={item2.Image} alt={item2.Name} />
+                          <a href={item2.entityViewUrl} target="_blank" rel="noreferrer">
+                            <img src={item2.iconUrl} alt={item2.name} />
                           </a>
                         </div>
                         <div>
-                          <strong><a href={item2.Url} target="_blank" rel="noreferrer">{item2.Name}</a></strong><br/>
+                          <strong><a href={item2.entityViewUrl} target="_blank" rel="noreferrer">{item2.name}</a></strong><br/>
                           <span className="location">
                             <span className="glyphicon glyphicon-map-marker"></span>
-                            {item.City}, {item.Country}
+                            {item2.positionLabel}
                           </span>
                         </div>
                       </div>;
                     })
                   }
                 </Popup>
-              </Marker>
+              </Marker>;
             })
           }
         </MapContainer>
