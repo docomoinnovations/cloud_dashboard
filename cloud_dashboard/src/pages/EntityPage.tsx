43c4c0217bc96d1cb25bd265450902d644f9a664
Ryo Yamashita <48265860+RyoYamashita765@users.noreply.github.com>
2021-11-15 18:21:57 +0900

    Change page selector icon (#34)
    * Change page selector icon
    
    * Centering of PageSelector
    
    * Add PageSelector above EntityTable
    
    * Fix to not display PageSelector when the number of pages is less than 1 page
    
    * Add scrolling to the top of the page when switching pages

--- cloud_dashboard/src/pages/EntityPage.tsx
+++ cloud_dashboard/src/pages/EntityPage.tsx
@@ -35,6 +35,10 @@ const EntityPage = ({ menuTemplate }: {
     }
   }, [namespaceName]);
 
+  useEffect(() => {
+    window.scrollBy(0, -window.pageYOffset);
+  }, [pageIndex]);
+
   const namespaceFlg = menuTemplate.entityColumn.map((c) => c.labelName).includes('Namespace');
   const namespaceNameFlg = menuTemplate.entityColumn.map((c) => c.labelName).includes('Namespace Name');
 
@@ -47,25 +51,25 @@ const EntityPage = ({ menuTemplate }: {
           <form>
             {
               namespaceFlg
-              ? <div className="form-group" style={{ marginTop: '2rem' }}>
+                ? <div className="form-group" style={{ marginTop: '2rem' }}>
                   <ControlLabel>Namespace</ControlLabel>
                   <FieldSelect
                     columnKey="namespace"
                     columnName={namespace}
                     setColumnName={setNamespace} />
-              </div>
-              : <></>
+                </div>
+                : <></>
             }
             {
               namespaceNameFlg
-              ? <div className="form-group" style={{ marginTop: '2rem' }}>
+                ? <div className="form-group" style={{ marginTop: '2rem' }}>
                   <ControlLabel>Namespace Name</ControlLabel>
                   <FieldSelect
                     columnKey="namespace_name"
                     columnName={namespaceName}
                     setColumnName={setNamespaceName} />
-              </div>
-              : <></>
+                </div>
+                : <></>
             }
             <div className="form-group" style={{ marginTop: '2rem' }}>
               <ItemCountLabel
@@ -75,6 +79,18 @@ const EntityPage = ({ menuTemplate }: {
                 itemCount={itemCount}
                 setItemCount={setItemCount} />
             </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <PageSelector
+      pageIndex={pageIndex}
+      setPageIndex={setPageIndex}
+      itemCount={itemCount} />
+    <div className="container-fluid px-0">
+      <div className="row mx-0">
+        <div className="col">
+          <form>
             <EntityTable
               entityTypeId={entityTypeId}
               entityColumnList={menuTemplate.entityColumn}
