b1a3e4c00dfd45770fdcf4fc0cc581143e221bee
Ryo Yamashita <yamashita@ipride.co.jp>
2021-11-17 18:36:06 +0900

    Add data for k8s_pod

--- cloud_dashboard/src/constant.ts
+++ cloud_dashboard/src/constant.ts
@@ -287,6 +287,7 @@ export const K8S_MENU_LIST: MenuTemplate[] = [
     cloudServiceProvider: 'k8s',
     labelName: 'Pod',
     entityName: 'pod',
+    detailInfoColumn: 'name',
     entityColumn: [
       { labelName: 'Namespace', name: 'namespace', type: 'default' },
       { labelName: 'Name', name: 'name', type: 'default' },
@@ -593,7 +594,61 @@ export const K8S_MENU_LIST: MenuTemplate[] = [
   },
 ];
 export const AWS_ENTITY_INFO_LIST: EntityInfoTemplate[] = [];
-export const K8S_ENTITY_INFO_LIST: EntityInfoTemplate[] = [];
+export const K8S_ENTITY_INFO_LIST: EntityInfoTemplate[] = [
+  {
+    cloudServiceProvider: 'k8s',
+    entityName: 'pod',
+    entityRecords: [
+      {
+        panelName: 'Pod',
+        tableRecordList: ['labels', 'annotations'],
+        keyValueRecords: [
+          { labelName: 'Name', name: 'name', type: 'default' },
+          { labelName: 'Labels', name: 'labels', type: 'default' },
+          { labelName: 'Annotations', name: 'annotations', type: 'default' },
+          { labelName: 'Namespace', name: 'namespace', type: 'default' },
+          { labelName: 'Status', name: 'status', type: 'default' },
+          { labelName: 'Qos Class', name: 'qos_class', type: 'default' },
+          { labelName: 'Node', name: 'node_name', type: 'default' },
+          { labelName: 'Pod IP', name: 'pod_ip', type: 'default' },
+        ]
+      },
+      {
+        panelName: 'Metrics',
+        tableRecordList: [],
+        keyValueRecords: [
+          { labelName: 'CPU (Request)', name: 'cpu_request', type: 'cpu' },
+          { labelName: 'CPU (Limit)', name: 'cpu_limit', type: 'cpu' },
+          { labelName: 'CPU (Usage)', name: 'cpu_usage', type: 'cpu' },
+          { labelName: 'Memory (Request)', name: 'memory_request', type: 'memory' },
+          { labelName: 'Memory (Limit)', name: 'memory_limit', type: 'memory' },
+          { labelName: 'Memory (Usage)', name: 'memory_usage', type: 'memory' },
+        ]
+      },
+      {
+        panelName: 'Containers',
+        tableRecordList: [],
+        keyValueRecords: [
+          { labelName: 'Containers', name: 'containers', type: 'array' },
+        ]
+      },
+      {
+        panelName: 'Detail',
+        tableRecordList: [],
+        keyValueRecords: [
+          { labelName: 'Detail', name: 'detail', type: 'default' },
+        ]
+      },
+      {
+        panelName: 'Other',
+        tableRecordList: [],
+        keyValueRecords: [
+          { labelName: 'Cloud Service Provider ID', name: 'cloud_context', type: 'default' },
+        ]
+      },
+    ]
+  }
+];
 export const ITEMS_PER_PAGE = 30;
 export const CACHE_EXPIRED_UNIXTIME = 1000 * 60 * 60 * 24;
 export const DEFAULT_CLOUD_CONTEXTS: CloudContext[] = [
