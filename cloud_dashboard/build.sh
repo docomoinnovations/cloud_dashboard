474959860da786cd238d126bd91d1e07c6fa9a6a
Ryo Yamashita <yamashita@ipride.co.jp>
2021-11-25 12:31:38 +0900

    Add coments in build.sh

--- cloud_dashboard/build.sh
+++ cloud_dashboard/build.sh
@@ -1,10 +1,15 @@
+# Build cloud_dashboard
 yarn react-scripts build
+
+# Remove old files
 rm ../js/2.chunk.js
 rm ../js/3.chunk.js
 rm ../js/main.chunk.js
 rm ../js/runtime-main.js
 rm ../css/2.chunk.css
 rm ../css/main.chunk.css
+
+# Copy new files from build directory
 cp build/static/js/2.*.chunk.js ../js/2.chunk.js
 cp build/static/js/3.*.chunk.js ../js/3.chunk.js
 cp build/static/js/main.*.chunk.js ../js/main.chunk.js
