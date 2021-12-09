2fa366ab9870452b04b6390fb7eb905e8d1e0db8
Ryo Yamashita <yamashita@ipride.co.jp>
2021-12-09 16:11:03 +0900

    Split aws_cloud.ts and k8s.ts

--- cloud_dashboard/src/constant/entity_info_template/entity_info_template.ts
+++ cloud_dashboard/src/constant/entity_info_template/entity_info_template.ts
@@ -1,6 +1,9 @@
-import AWS_ENTITY_INFO_LIST from 'constant/entity_info_template/aws_cloud';
-import K8S_ENTITY_INFO_LIST from 'constant/entity_info_template/k8s';
+import AWS_CLOUD_INSTANCE_TEMPLATE from 'constant/entity_info_template/aws_cloud/instance';
+import K8S_POD_TEMPLATE from 'constant/entity_info_template/k8s/pod';
 
-const ENTITY_INFO_LIST = [...AWS_ENTITY_INFO_LIST, ...K8S_ENTITY_INFO_LIST];
+const ENTITY_INFO_LIST = [
+  AWS_CLOUD_INSTANCE_TEMPLATE,
+  K8S_POD_TEMPLATE,
+];
 
 export default ENTITY_INFO_LIST;
