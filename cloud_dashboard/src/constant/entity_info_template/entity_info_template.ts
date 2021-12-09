235ddc5aeb5930613f532f2d7423a44d2a73674e
Ryo Yamashita <48265860+RyoYamashita765@users.noreply.github.com>
2021-12-09 19:05:24 +0900

    Split entity info template (#56)
    * Split entity_info_template.ts to aws_cloud.ts and k8s.ts
    
    * Split aws_cloud.ts and k8s.ts

---
+++ cloud_dashboard/src/constant/entity_info_template/entity_info_template.ts
@@ -0,0 +1,9 @@
+import AWS_CLOUD_INSTANCE_TEMPLATE from 'constant/entity_info_template/aws_cloud/instance';
+import K8S_POD_TEMPLATE from 'constant/entity_info_template/k8s/pod';
+
+const ENTITY_INFO_LIST = [
+  AWS_CLOUD_INSTANCE_TEMPLATE,
+  K8S_POD_TEMPLATE,
+];
+
+export default ENTITY_INFO_LIST;
